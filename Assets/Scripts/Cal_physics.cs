9e8477c6645889aee90513d5186cb38165de04d8
furioustank986 <51895478+furioustank986@users.noreply.github.com>
2021-05-20 14:04:58 +0900

    Add files via upload

--- Assets/Scripts/Cal_physics.cs
+++ Assets/Scripts/Cal_physics.cs
@@ -1,1 +1,62 @@
-zzz
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cal_physics : MonoBehaviour
+{
+    public Material fastWheelMaterial;
+    public Material slowWheelMaterial;
+    public Rigidbody rb;
+    public MeshRenderer rend;
+    float ang_velocity;
+    public Text velocityText;
+    public Vector3 com;
+    public Text massText;
+    public float R_radius_distance;
+    private float Centerforce_re;
+    private float mass;
+    public Text Centerforce;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<MeshRenderer>();
+        rb.centerOfMass = com;
+        velocityText.text = "Angular Velocity: " + ang_velocity;
+
+        mass = rb.mass;
+        massText.text = "Rocket Mass: " + mass;
+        Centerforce.text = "Center Forece: " + Centerforce_re;
+
+    }
+   
+    void Update()
+    {
+         
+
+        if (rb.angularVelocity.magnitude < 5)
+        {
+            rend.sharedMaterial = slowWheelMaterial;
+            ang_velocity = rb.angularVelocity.magnitude;
+            //Debug.Log("velocity < 5 :" + ang_velocity);
+            velocityText.text = "Angular Velocity: " + ang_velocity;
+            var Centerforce_re = mass * R_radius_distance * ang_velocity * ang_velocity;
+            //Debug.Log("CenterForece:" + Centerforce);
+            Centerforce.text = "Center Forece: " + Centerforce_re;
+
+        }
+        else
+        {
+            rend.sharedMaterial = fastWheelMaterial;
+            ang_velocity = rb.angularVelocity.magnitude;
+            //Debug.Log("velocity > 5 :" + ang_velocity);
+            velocityText.text = "Angular Velocity: " + ang_velocity;
+            var Centerforce_re = mass * R_radius_distance * ang_velocity * ang_velocity;
+            //Debug.Log("CenterForece:" + Centerforce);
+            Centerforce.text = "Center Forece: " + Centerforce_re;
+        }
+
+    }
+}
---
+++ Assets/Scripts/spin_launcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spin_launcher : MonoBehaviour
+{
+
+    public GameObject motor;
+    public float speed;
+    // public float width;
+    // public float height;
+    public Text MotorSpeed;    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform rectTransform;
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spin_around();
+        MotorSpeed.text = "Motor Speed: " + speed;
+        
+    }
+
+    void spin_around()
+    {
+        transform.RotateAround(motor.transform.position, Vector3.down, speed * Time.deltaTime);
+    }
+}
